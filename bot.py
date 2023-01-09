9c2204a742224cae2d34d274dddb0d55c506bcce
Firlej <25161992+Firlej@users.noreply.github.com>
2023-01-09 17:03:46 +0000

    working with scaling percentage margin

--- bot.py
+++ bot.py
@@ -18,8 +18,28 @@ exchange = ccxt.binance({
 symbol = 'BTC/BUSD'
 
 budget = 10.25
-wait_time = 60 * 3  # wait for 2 hours before placing the next trade
-sell_percentage = 1.001  # sell the bitcoin bought in the market buy order for this percentage more
+wait_before_new_market_order_sec = 60
+
+sell_percentage_min = 1.00001
+sell_percentage_max = 1.001
+
+def map_range(x, a, b, y, z):
+    return (x - a) * (z - y) / (b - a) + y
+
+def get_sell_percentage():
+    
+    # retrieve all open orders
+    open_orders = exchange.fetch_open_orders(symbol=symbol)
+    # calculate the total BUSD in open orders
+    total_open_sell_busd = sum([order["amount"] * order["price"] for order in open_orders])
+
+    # map the sell percentage to be inversely proportional to the total BUSD in the account and open sell orders
+    balance = exchange.fetch_balance()
+    available_busd = balance['BUSD']['free']
+
+    total_busd = available_busd + total_open_sell_busd
+    sell_percentage = map_range(available_busd, 0, total_busd, sell_percentage_min, sell_percentage_max)
+    return max(sell_percentage, sell_percentage_min)
 
 def log_trade(order):
     # log completed market trades and open/completed limit orders to a CSV file
@@ -43,50 +63,96 @@ def log_trade(order):
         writer.writerow(d)
 
 
-def buy(budget):
+def market_buy(budget):
     # place a market buy order for the specified budget
     order = exchange.create_order(symbol, 'market', 'buy', None, None, {'cost': budget})
-    # print(f'Placed market buy order for {budget} BUSD')
-    return order
+    log_trade(order)  # log completed market buy
+    limit_sell(order)
 
-def sell(order):
-    # sell the bitcoin bought in the market buy order for the specified percentage more
-    sell_price = order['price'] * sell_percentage
+def limit_sell(order):
+    sell_price = order['price'] * get_sell_percentage()
     sell_amount = order['filled']
     sell_order = exchange.create_order(symbol, 'limit', 'sell', sell_amount, sell_price)
-    return sell_order
+    log_trade(sell_order)  # log limit sell opened
 
-def check_for_completed_sell_orders(sell_order):
-    # check if the limit sell order has been completed every 5 seconds
-    timer = 5
-    while True:
-        order = exchange.fetch_order(sell_order['id'], symbol)  # pass the symbol argument
-        if order['status'] == 'closed':
-            log_trade(order)  # log completed limit order
-            break
-        if timer < 60:
-            timer += 1
-        time.sleep(timer)
+    # create a separate thread for checking for completed limit sell orders
+    thread = threading.Thread(target=check_for_completed_order, args=(sell_order,))
+    thread.start()
 
-while True:
+def limit_buy(order):
+
+    cancel_buy_orders()
+
+    buy_price = order['price'] / get_sell_percentage()
+    buy_amount = order['filled']
+    
     # check the available balance of BUSD in the account
     balance = exchange.fetch_balance()
     available_busd = balance['BUSD']['free']
 
-    if available_busd < budget:
-        print(f'Not enough BUSD in the account. Available balance: {available_busd} BUSD')
-    else:
-        # buy $11 worth of bitcoin
-        initial_buy_order = buy(budget)
-        log_trade(initial_buy_order)  # log completed market trade
+    if available_busd < buy_price * buy_amount:
+        return
+    
+    buy_order = exchange.create_order(symbol, 'limit', 'buy', buy_amount, buy_price)
+    # create a separate thread for checking for completed orders
+    log_trade(buy_order)  # log limit opened opened
+    thread = threading.Thread(target=check_for_completed_order, args=(buy_order,))
+    thread.start()
 
-        # sell the bitcoin bought in the market buy order for 0.01% more
-        sell_order = sell(initial_buy_order)
-        log_trade(sell_order)  # log open limit order
 
-        # create a separate thread for checking for completed limit sell orders
-        thread = threading.Thread(target=check_for_completed_sell_orders, args=(sell_order,))
-        thread.start()
 
-    # wait
-    time.sleep(wait_time)
+def check_for_completed_order(order):
+    # check if the limit sell order has been completed every 5 seconds
+    timer = 5
+    while True:
+        order = exchange.fetch_order(order['id'], symbol)  # pass the symbol argument
+        # print(order["id"], order['status'])
+        if order['status'] == 'closed':
+            log_trade(order)
+            if (order['side'] == "buy"):
+                limit_sell(order)
+            else:
+                limit_buy(order)
+            return
+        elif order['status'] == "canceled":
+            return
+        if timer < 60:
+            timer += 1
+        time.sleep(timer)
+
+
+def cancel_buy_orders():
+    # Get a list of open orders
+    open_orders = exchange.fetch_open_orders(symbol=symbol)
+
+    # Iterate through the open orders and cancel only buy orders
+    for order in open_orders:
+        if order['side'] == 'buy':
+            exchange.cancel_order(order['id'], symbol=symbol)
+
+def get_seconds_since_last_trade() -> float:
+    trades = exchange.fetch_my_trades(symbol, limit=1)
+    last_trade_timestamp = trades[0]['timestamp'] // 1000
+    current_time = time.time()
+    elapsed_time = current_time - last_trade_timestamp
+    return elapsed_time
+
+
+while True:
+
+    seconds_since_last_trade = get_seconds_since_last_trade()
+    if seconds_since_last_trade > wait_before_new_market_order_sec:
+
+        # check the available balance of BUSD in the account
+        balance = exchange.fetch_balance()
+        available_busd = balance['BUSD']['free']
+
+        if available_busd < budget:
+            print(f'Not enough BUSD in the account. Available balance: {available_busd} BUSD')
+        else:
+            # buy `budget` worth of bitcoin
+            initial_buy_order = market_buy(budget)
+        
+        seconds_since_last_trade = 0
+
+    time.sleep(wait_before_new_market_order_sec - seconds_since_last_trade + 1)
