7ece24f5d07ef6d2ee7a2556c0ea69c51b4e0039
Firlej <25161992+Firlej@users.noreply.github.com>
2023-01-16 10:46:22 +0000

    add comments

--- bot.py
+++ bot.py
@@ -25,9 +25,11 @@ SLEEP_MAX = 60 * 10
 PROFIT_MARGIN_MIN = 1.00001
 PROFIT_MARGIN_MAX = 1.001
 
+# map a range of values to another range of values
 def map_range(x, a, b, y, z):
     return (x - a) * (z - y) / (b - a) + y
 
+# get current free and total BUSD balance
 def get_busd_balances():
 	# retrieve all open orders
 	open_orders = exchange.fetch_open_orders(symbol=symbol)
@@ -39,20 +41,22 @@ def get_busd_balances():
 	total_busd = free_busd + total_open_order_busd
 	return free_busd, total_busd
 
+# get the sell percentage based on the current BUSD balance
 def get_sell_percentage():
 	free_busd, total_busd = get_busd_balances()
 	sell_percentage = map_range(free_busd, 0, total_busd, PROFIT_MARGIN_MIN, PROFIT_MARGIN_MAX)
 	assert sell_percentage >= 1
 	return sell_percentage
 
+# get the sleep time based on the current BUSD balance
 def get_sleep_scaled():
 	free_busd, total_busd = get_busd_balances()
 	sleep_scaled = map_range(free_busd, 0, total_busd, SLEEP_MAX, SLEEP_MIN)
 	assert sleep_scaled >= SLEEP_MIN
 	return sleep_scaled
 
+# log completed market trades and open/completed limit orders to a CSV file
 def log_trade(order):
-    # log completed market trades and open/completed limit orders to a CSV file
     with open('trades.csv', 'a', newline='') as csvfile:
         fieldnames = ['timestamp', 'type', 'symbol', 'side', 'price', 'amount', 'cost', 'order_id', 'status']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
@@ -72,12 +76,14 @@ def log_trade(order):
         print(f'{order["side"]} order for {order["amount"]} BTC at a price of {order["price"]} for a value of {(order["price"] * order["amount"])}. status: {order["status"]}')
         writer.writerow(d)
 
+# place a market buy order for the specified budget
 def market_buy(budget):
     # place a market buy order for the specified budget
     order = exchange.create_order(symbol, 'market', 'buy', None, None, {'cost': budget})
     log_trade(order)  # log completed market buy
     limit_sell(order)
 
+# place a limit sell order for the amount of BTC that was bought
 def limit_sell(order):
     sell_price = order['price'] * get_sell_percentage()
     sell_amount = order['filled']
@@ -88,6 +94,7 @@ def limit_sell(order):
     thread = threading.Thread(target=check_for_completed_order, args=(sell_order,))
     thread.start()
 
+# place a limit buy order for the amount of BTC that was sold
 def limit_buy(order):
 
     cancel_buy_orders()
@@ -108,6 +115,7 @@ def limit_buy(order):
     thread = threading.Thread(target=check_for_completed_order, args=(buy_order,))
     thread.start()
 
+# periodically check for completed order
 def check_for_completed_order(order):
     # check if the limit sell order has been completed every 5 seconds
     timer = 5
@@ -136,6 +144,7 @@ def cancel_buy_orders():
         if order['side'] == 'buy':
             exchange.cancel_order(order['id'], symbol=symbol)
 
+# get the number of seconds since the last trade
 def get_seconds_since_last_trade() -> float:
     trades = exchange.fetch_my_trades(symbol, limit=1)
     last_trade_timestamp = trades[0]['timestamp'] // 1000
@@ -143,7 +152,7 @@ def get_seconds_since_last_trade() -> float:
     elapsed_time = current_time - last_trade_timestamp
     return elapsed_time
 
-
+# main loop
 while True:
 
 	seconds_since_last_trade = get_seconds_since_last_trade()
