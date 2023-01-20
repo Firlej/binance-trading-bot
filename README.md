d02b9c8a4b9bbf0a29d6ef9c4d825f1e67556a44
Firlej <25161992+Firlej@users.noreply.github.com>
2023-01-20 20:52:26 +0000

    refactored, but has bugs

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # binance-trading-bot
 
+
+### Setup
+
+Create `.env` file based on `.env.example` file
+
+```
+cp .env.example .env
+```
+
 ```
 rm -rf venv && python3 -m venv venv && source venv/bin/activate && pip install -r requirements.txt
 ```
 
+```
+pip freeze > requirements.txt
+```
--- bot.py
+++ bot.py
@@ -10,20 +10,47 @@ import json
 load_dotenv()
 
 # define the exchange and the markets you want to trade on
-exchange = ccxt.binance({
-    'apiKey': os.getenv('API_KEY'),
-    'secret': os.getenv('API_SECRET')
-})
+exchange = ccxt.binance(
+    {"apiKey": os.getenv("API_KEY"), "secret": os.getenv("API_SECRET")}
+)
 
-symbol = 'BTC/BUSD'
+symbol = "BTC/BUSD"
 
 budget = 10.25
 
 SLEEP_MIN = 60
-SLEEP_MAX = 60 * 10
+SLEEP_MAX = 60 * 20
 
 PROFIT_MARGIN_MIN = 1.00001
-PROFIT_MARGIN_MAX = 1.001
+PROFIT_MARGIN_MAX = 1.002
+
+last_trade_timestamp = 0
+
+# free_busd, total_busd = 0, 0
+
+# get the number of seconds since the last trade
+def get_seconds_since_last_trade():
+    global last_trade_timestamp
+    if last_trade_timestamp == 0:
+        return 0
+    return time.time() - last_trade_timestamp
+
+
+# update last trade timestamp
+def update_last_trade_timestamp():
+    last_trade = exchange.fetch_my_trades(symbol, limit=1)[0]
+    global last_trade_timestamp
+    last_trade_timestamp = last_trade["timestamp"] // 1000
+
+
+# periodically update the last trade timestamp
+def update_last_trade_timestamp_thread():
+	while True:
+		update_last_trade_timestamp()
+		time.sleep(5)
+
+# start the thread to update the last trade timestamp
+threading.Thread(target=update_last_trade_timestamp_thread).start()
 
 # map a range of values to another range of values
 def map_range(x, a, b, y, z):
@@ -31,145 +58,193 @@ def map_range(x, a, b, y, z):
 
 # get current free and total BUSD balance
 def get_busd_balances():
-	# retrieve all open orders
-	open_orders = exchange.fetch_open_orders(symbol=symbol)
-	# calculate the total BUSD in open orders
-	total_open_order_busd = sum([order["amount"] * order["price"] for order in open_orders])
+    # retrieve all open orders
+    open_orders = exchange.fetch_open_orders(symbol=symbol)
+    # calculate the total BUSD in open orders
+    total_open_order_busd = sum([order["amount"] * order["price"] for order in open_orders])
 
-	balance = exchange.fetch_balance()
-	free_busd = balance['BUSD']['free']
-	total_busd = free_busd + total_open_order_busd
-	return free_busd, total_busd
+    balance = exchange.fetch_balance()
+    free_busd = balance['BUSD']['free']
+    total_busd = free_busd + total_open_order_busd
+    return free_busd, total_busd
 
 # get the sell percentage based on the current BUSD balance
 def get_sell_percentage():
-	free_busd, total_busd = get_busd_balances()
-	sell_percentage = map_range(free_busd, 0, total_busd, PROFIT_MARGIN_MIN, PROFIT_MARGIN_MAX)
-	assert sell_percentage >= 1
-	return sell_percentage
+    free_busd, total_busd = get_busd_balances()
+    sell_percentage = map_range(
+        free_busd, 0, total_busd, PROFIT_MARGIN_MIN, PROFIT_MARGIN_MAX
+    )
+    assert sell_percentage >= 1
+    return sell_percentage
 
 # get the sleep time based on the current BUSD balance
 def get_sleep_scaled():
-	free_busd, total_busd = get_busd_balances()
-	sleep_scaled = map_range(free_busd, 0, total_busd, SLEEP_MAX, SLEEP_MIN)
-	assert sleep_scaled >= SLEEP_MIN
-	return sleep_scaled
+    free_busd, total_busd = get_busd_balances()
+    sleep_scaled = map_range(free_busd, 0, total_busd, SLEEP_MAX, SLEEP_MIN)
+    assert sleep_scaled >= SLEEP_MIN
+    return sleep_scaled
 
 # log completed market trades and open/completed limit orders to a CSV file
 def log_trade(order):
-    with open('trades.csv', 'a', newline='') as csvfile:
-        fieldnames = ['timestamp', 'type', 'symbol', 'side', 'price', 'amount', 'cost', 'order_id', 'status']
+    with open("trades.csv", "a", newline="") as csvfile:
+        fieldnames = [
+            "timestamp",
+            "type",
+            "symbol",
+            "side",
+            "price",
+            "amount",
+            "cost",
+            "order_id",
+            "status",
+        ]
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
         d = {
-            'timestamp': order['timestamp'],
-            'type': order['type'],
-            'symbol': order['symbol'],
-            'side': order['side'],
-            'price': order['price'],
-            'amount': order['amount'] if order['amount'] else order['filled'],
-            'cost': order['price'] * order['amount'],  # calculate cost as price * amount
-            'order_id': order['id'],  # add order id to log
-            'status': order['status']
+            "timestamp": order["timestamp"],
+            "type": order["type"],
+            "symbol": order["symbol"],
+            "side": order["side"],
+            "price": order["price"],
+            "amount": order["amount"] if order["amount"] else order["filled"],
+            "cost": order["price"] * order["amount"],
+            "order_id": order["id"],
+            "status": order["status"],
         }
 
-        print(f'{order["side"]} order for {order["amount"]} BTC at a price of {order["price"]} for a value of {(order["price"] * order["amount"])}. status: {order["status"]}')
+        print(
+            f'{order["side"]} order for {order["amount"]} BTC at a price of {order["price"]} for a value of {(order["price"] * order["amount"])}. status: {order["status"]}'
+        )
         writer.writerow(d)
+
 
 # place a market buy order for the specified budget
 def market_buy(budget):
-    # place a market buy order for the specified budget
-    order = exchange.create_order(symbol, 'market', 'buy', None, None, {'cost': budget})
-    log_trade(order)  # log completed market buy
-    limit_sell(order)
+
+    try:
+        # place a market buy order for the specified budget
+        order = exchange.create_order(
+            symbol, "market", "buy", None, None, {"cost": budget}
+        )
+
+        # update last trade timestamp
+        global last_trade_timestamp
+        last_trade_timestamp = time.time()
+
+        # log the market buy order
+        log_trade(order)
+
+        # place a limit sell order for the amount of BTC that was bought
+        limit_sell(order)
+
+    except ccxt.errors.InsufficientFunds:
+        print("Insufficient funds")
+        return
+
 
 # place a limit sell order for the amount of BTC that was bought
 def limit_sell(order):
-    sell_price = order['price'] * get_sell_percentage()
-    sell_amount = order['filled']
-    sell_order = exchange.create_order(symbol, 'limit', 'sell', sell_amount, sell_price)
+    sell_price = order["price"] * get_sell_percentage()
+    sell_amount = order["filled"]
+    sell_order = exchange.create_order(symbol, "limit", "sell", sell_amount, sell_price)
     log_trade(sell_order)  # log limit sell opened
 
     # create a separate thread for checking for completed limit sell orders
-    thread = threading.Thread(target=check_for_completed_order, args=(sell_order,))
-    thread.start()
+    threading.Thread(target=check_for_completed_order, args=(sell_order,)).start()
+
 
 # place a limit buy order for the amount of BTC that was sold
 def limit_buy(order):
 
-    cancel_buy_orders()
-
-    buy_price = order['price'] / get_sell_percentage()
-    buy_amount = order['filled']
-    
-    # check the available balance of BUSD in the account
-    balance = exchange.fetch_balance()
-    available_busd = balance['BUSD']['free']
-
-    if available_busd < buy_price * buy_amount:
-        return
-    
-    buy_order = exchange.create_order(symbol, 'limit', 'buy', buy_amount, buy_price)
-    # create a separate thread for checking for completed orders
-    log_trade(buy_order)  # log limit opened opened
-    thread = threading.Thread(target=check_for_completed_order, args=(buy_order,))
+    # exectute cancel_buy_orders() in a seperate thread
+    thread = threading.Thread(target=cancel_buy_orders)
     thread.start()
+
+    buy_price = order["price"] / get_sell_percentage()
+    buy_amount = order["filled"]
+
+    try:
+        buy_order = exchange.create_order(symbol, "limit", "buy", buy_amount, buy_price)
+
+        # create a separate thread for checking for completed orders
+        log_trade(buy_order)  # log limit opened opened
+
+        # create a separate thread for checking for completed limit orders
+        threading.Thread(target=check_for_completed_order, args=(buy_order,)).start()
+
+    except ccxt.errors.InsufficientFunds:
+        print("Insufficient funds")
+        return
+
 
 # periodically check for completed order
 def check_for_completed_order(order):
     # check if the limit sell order has been completed every 5 seconds
-    timer = 5
+    timer = 1
     while True:
-        order = exchange.fetch_order(order['id'], symbol)  # pass the symbol argument
+        order = exchange.fetch_order(order["id"], symbol)  # pass the symbol argument
         # print(order["id"], order['status'])
-        if order['status'] == 'closed':
+        if order["status"] == "closed":
             log_trade(order)
-            if (order['side'] == "buy"):
+            if order["side"] == "buy":
                 limit_sell(order)
             else:
                 limit_buy(order)
             return
-        elif order['status'] == "canceled":
+        elif order["status"] == "canceled":
             return
         if timer < 60:
             timer += 1
         time.sleep(timer)
 
+
+# cancel all open buy orders except the one with the highest buy price
 def cancel_buy_orders():
-    # Get a list of open orders
+
+    # get all open orders
     open_orders = exchange.fetch_open_orders(symbol=symbol)
 
-    # Iterate through the open orders and cancel only buy orders
-    for order in open_orders:
-        if order['side'] == 'buy':
-            exchange.cancel_order(order['id'], symbol=symbol)
+    # filter all open buy orders
+    open_buy_orders = [order for order in open_orders if order["side"] == "buy"]
 
-# get the number of seconds since the last trade
-def get_seconds_since_last_trade() -> float:
-    trades = exchange.fetch_my_trades(symbol, limit=1)
-    last_trade_timestamp = trades[0]['timestamp'] // 1000
-    current_time = time.time()
-    elapsed_time = current_time - last_trade_timestamp
-    return elapsed_time
+    if len(open_buy_orders) == 0:
+        return
+
+    # find the order with highest buy price
+    order_max = max(open_buy_orders, key=lambda x: x["price"])
+
+    # cancel all open buy orders except the one with the highest buy price
+    for order in open_buy_orders:
+
+        if order["id"] == order_max["id"]:
+            continue
+
+        try:
+            exchange.cancel_order(order["id"], symbol=symbol)
+        except ccxt.errors.OrderNotFound:
+            pass
+
+update_last_trade_timestamp()
+
 
 # main loop
 while True:
 
-	seconds_since_last_trade = get_seconds_since_last_trade()
-	sleep_timer = get_sleep_scaled()
-	if seconds_since_last_trade > sleep_timer:
+    seconds_since_last_trade = get_seconds_since_last_trade()
+    sleep_timer = get_sleep_scaled()
+    if seconds_since_last_trade > sleep_timer:
 
-		# check the available balance of BUSD in the account
-		balance = exchange.fetch_balance()
-		available_busd = balance['BUSD']['free']
+        # check the available balance of BUSD in the account
+        balance = exchange.fetch_balance()
+        available_busd = balance["BUSD"]["free"]
 
-		if available_busd < budget:
-			print(f'Not enough BUSD in the account. Available balance: {available_busd} BUSD')
-		else:
-			# buy `budget` worth of bitcoin
-			initial_buy_order = market_buy(budget)
-		
-		seconds_since_last_trade = 0
+        if available_busd < budget:
+            print(f"Not enough BUSD in the account. Available balance: {available_busd} BUSD")
+        else:
+            # buy `budget` worth of bitcoin
+            initial_buy_order = market_buy(budget)
 
-	print("sleeping for: ", sleep_timer - seconds_since_last_trade + 1)
-	time.sleep(sleep_timer - seconds_since_last_trade + 1)
+        seconds_since_last_trade = 0
+
+    print("sleeping for: ", sleep_timer - seconds_since_last_trade + 1)
+    time.sleep(sleep_timer - seconds_since_last_trade + 1)
